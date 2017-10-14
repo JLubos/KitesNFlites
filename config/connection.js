0b83b38be85990b630913e17ceb1da4a2d7fc794
JLubos <juslubos@yahoo.com>
2017-10-14 10:45:34 -0500

    orm and connncetion

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,22 @@
+// Require mysql
 var mysql = require("mysql");
 
+// Set up our connection information
 var connection = mysql.createConnection({
-	port: 3000,
-	host: "localhost",
-	password: "rootpassword",
-	database: "Kites_db"
-
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "rootpassword",
+  database: "Kites_db"
 });
 
-
+// Connect to the database
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
-  
-}  console.log("connected as id " + connection.threadId);
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection
---
+++ config/orm.js
@@ -0,0 +1,26 @@
+var connection = require('./connection.js');
+
+var orm = {
+	all: function(tableInput, cb){
+		connection.query('SELECT * FROM ' + tableInput + ':', function(err,result){
+			if(err) throw err;
+			cb(result)
+		})
+	},
+
+	update: function(tableInput, condition, cb){
+		connection.query('UPDATE ' + tableInput+ 'WHERE id= '+condition+';', function(err,result){
+			if (err) throw err;
+			cb(result);
+		})
+	},
+	create: function(tableInput, val, cb){
+		connection.query('INSERT INTO ' + tableInput+"(parkName) VALUES ('"+val+"');",
+			function(err,result){
+				if (err) throw err;
+				cb(result);
+			}
+	}
+}
+
+module.exports = orm;
